50d66a30bee9f0e0e17ce9674ac82f6fb8f201b6
João Victor Arruda <jvanunes1@gmail.com>
2021-11-06 13:30:57 -0300

    finish project

--- public/js/behaviors.js
+++ public/js/behaviors.js
@@ -1,25 +1,26 @@
+//timer identifier
 let typingTimer;
+//time in ms, 1 second
 let doneTypingInterval = 1000;
 let editor = document.getElementById("editor");
 let pigEditor = document.getElementById("pig-editor");
 let speakingPig = document.getElementById("speaking-pig");
 
-
+//on keyup, start the countdown
 document.getElementById("editor").addEventListener("keyup", function() {
     clearTimeout(typingTimer);
     typingTimer = setTimeout(doneTyping, doneTypingInterval);
 }, false);
 
+//on keydown, clear the countdown
 document.getElementById("editor").addEventListener("keydown", function() {
     clearTimeout(typingTimer);
-    // speakingPig.setAttribute("src", "img/pig_1.png")
     speakingPig.setAttribute("style", "transform: scaleX(" + 1 + ")");
 
 }, false);
 
-//user is "finished typing," do something
+//user is "finished typing,"
 function doneTyping () {
     pigEditor.innerHTML = translate(editor.innerHTML);
-    // speakingPig.setAttribute("src", "img/pig_2.png")
     speakingPig.setAttribute("style", "transform: scaleX(" + -1 + ")");
 }
--- public/js/translator.js
+++ public/js/translator.js
@@ -1,3 +1,7 @@
+/**
+ * Translate the string to pig latin
+ * @param {String} str - The word that will be capitalized
+ */
 function translate(str) {
     str = str.trim();
     let vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
@@ -6,56 +10,80 @@ function translate(str) {
     let stem = '';
     let isUpperCase = false;
     let result = [];
+	
     for (let i = 0; i < words.length; i++) {
         if(hasJustNumbers(words[i])) {
             result.push(words[i]);
             continue;
         }
+		
         prefix = '';
         stem = '';
         isUpperCase = words[i][0] === words[i][0].toUpperCase();
         let word = words[i].toLowerCase();
         let foundVowel = false;
         let punctuation = '';
+		
         if(!isCharacterALetter(word.substr(word.length - 1))) {
             punctuation = word.substr(word.length - 1);
             word = word.slice(0, -1);
         }
+		
         for (let j = 0; j < word.length; j++) {
             if(vowels.includes(word[j])) {
                 foundVowel = true;
             }
+			
             if(!foundVowel) {
                 prefix += word[j];
             } else {
                 stem += word[j];
             }
         }
+		
         if(hasNoConsonants(word)) {
             prefix += 'y';
         }
+		
         stem = isUpperCase ? capitalizeFirstLetter(stem) : stem;
         result.push(stem + prefix + 'ay' + punctuation);
     }
     return result.join(' ');
 }
 
+/**
+ * Make the first letter be upper case
+ * @param {String} string - The word that will be capitalized
+ */
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+/**
+ * Verify if has no numbers and special characters
+ * @param {String} char - The word that will be verified
+ */
 function isCharacterALetter(char) {
     return (/[a-zA-Z]/).test(char)
 }
 
+/**
+ * Verify if has just numbers
+ * @param {String} string - The word that will be verified
+ */
 function hasJustNumbers(string) {
     return (/[0-9]/).test(string)
 }
 
+/**
+ * Verify if has no consonants
+ * @param {String} string - The word that will be verified
+ */
 function hasNoConsonants(string) {
     return !(/[bcdfghjklmnpqrstvwxyz]/gi).test(string)
 }
 
+/*
 const strings = [
     ['Stop', ['Opstay']],
     ['No littering', ['Onay itteringlay']],
@@ -70,5 +98,7 @@ strings.forEach(([str, cor]) => {
     const ans = translate(str)
     const isCor = cor.includes(ans)
     elem.innerHTML = `${isCor ? "OK" : "FAILED"} ${str} --> ${ans} ${isCor ? "" : "expected " + cor}`
-    // document.body.appendChild(elem)
+    document.body.appendChild(elem)
 });
+
+ */
